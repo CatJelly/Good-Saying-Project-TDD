f2e4b37d9a7a38bafa71b59789bb78425931c5f5
CatJelly <mej1406@gmail.com>
2023-03-06 15:30:02 +0900

    enhancement/1 : 요: 요구사항1 완료, 테스트 포함

--- Good-Saying-Project-TDD/src/main/java/com/ll/App.java
+++ Good-Saying-Project-TDD/src/main/java/com/ll/App.java
@@ -10,9 +10,17 @@ public class App {
     }
 
     public void run() {
+        System.out.println("== 명언 앱 ==");
         while(true) {
-            System.out.println("== 명언 앱 ==");
-            System.out.println("명령) ");
+            System.out.print("명령) ");
+            String cmd = sc.nextLine().trim();
+
+            if (cmd.equals("종료")) {
+                System.out.println("프로그램이 종료되었습니다.");
+                break;
+            } else {
+                System.out.println("올바르지 않은 명령입니다.");
+            }
         }
     }
 }
--- Good-Saying-Project-TDD/src/test/java/com/ll/AppTests.java
+++ Good-Saying-Project-TDD/src/test/java/com/ll/AppTests.java
@@ -3,10 +3,56 @@ package com.ll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
 public class AppTests {
     @Test
-    @DisplayName("명언 앱 타이틀 출력 및 커맨드 라인 출력")
+    @DisplayName("스캐너에 키보드가 아닌 문자열을 입력으로 설정")
+    public void t0() {
+        Scanner sc = TestUtil.genScanner("안녕");
+
+        String input = sc.nextLine().trim();
+        assertThat(input).isEqualTo("안녕");
+    }
+
+    @Test
+    @DisplayName("출력을 모니터가 아닌 문자열로 얻기")
     public void t1() {
-        asertThat()
+        // System.out 에 대한 화면출력 금지 시작
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        System.out.print("안녕");
+
+        // 그 동안 출력되지 않던 내용들을 문자열로 반환
+        String output = outputStream.toString();
+
+        // System.out 에 대한 화면출력 금지 끝
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        assertThat(output).isEqualTo("안녕");
+    }
+
+    @Test
+    @DisplayName("명언 앱 타이틀 출력 및 커맨드 라인 출력")
+    public void t2() {
+        String rs = AppTestRunner.run("");
+        assertThat(rs)
+                .contains("== 명언 앱 ==")
+                .contains("명령) ")
+                .contains("프로그램이 종료되었습니다.")
+                .doesNotContain("올바르지 않은 명령입니다.");
+    }
+
+    @Test
+    @DisplayName("잘못된 명령어 입력에 대한 처리")
+    public void t3() {
+        String rs = AppTestRunner.run("종료2");
+
+        assertThat(rs)
+                .contains("올바르지 않은 명령입니다.");
     }
 }
