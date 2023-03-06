69704d79cfdf1da435929af8f814c8c1884a8725
CatJelly <mej1406@gmail.com>
2023-03-06 14:59:44 +0900

    main : initial setting

---
+++ Good-Saying-Project-TDD/src/main/java/com/ll/App.java
@@ -0,0 +1,18 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class App {
+    Scanner sc;
+
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+        while(true) {
+            System.out.println("== 명언 앱 ==");
+            System.out.println("명령) ");
+        }
+    }
+}
---
+++ Good-Saying-Project-TDD/src/test/java/com/ll/AppTests.java
@@ -0,0 +1,12 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTests {
+    @Test
+    @DisplayName("명언 앱 타이틀 출력 및 커맨드 라인 출력")
+    public void t1() {
+        asertThat()
+    }
+}
